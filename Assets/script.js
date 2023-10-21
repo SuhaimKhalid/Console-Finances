2ddb03284fa5d17f37694633af9699413c9f1cb4
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-10-21 19:04:02 +0100

    Add starter code

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+// * The total number of months included in the dataset.
+
+// * The net total amount of Profit/Losses over the entire period.
+
+// * The average of the **changes** in Profit/Losses over the entire period.
+//   * You will need to track what the total change in Profit/Losses are from month to month and then find the average.
+//   * (`Total/(Number of months - 1)`)
+
+// * The greatest increase in Profit/Losses (date and amount) over the entire period.
+
+// * The greatest decrease in Profit/Losses (date and amount) over the entire period.
+
+// When you open your code in the browser your resulting analysis should look similar to the following:
+
+//   ```text
+//   Financial Analysis 
+//   ----------------
+//   Total Months: 86
+//   Total: $38382578
+//   Average Change: -2315.12
+//   Greatest Increase in Profits/Losses: Feb-2012 ($1926159)
+//   Greatest Decrease in Profits/Losses: Sep-2013 ($-2196167)
+//   ```
+
+// Your final code should print the analysis to the console.
